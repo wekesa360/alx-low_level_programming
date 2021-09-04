30aea478f05b032cdd160a42b07474fb10a3f99f
Michael Wekesa <wekesa360@outlook.com>
2021-09-04 15:03:36 +0300

    Development: Debugging project

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - tests for a function that prints if a number is positive or 
+ *          negative.
+ * Return value: o
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+In this project, we debug some C files
