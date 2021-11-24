db8daaf58651d9688cfde6da5f6bc4aea34bf3a7
Michael Wekesa <wekesa360@outlook.com>
2021-11-24 23:40:12 +0300

    Fix: 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+
+#!/usr/bin/python3
+
+""" Interview preparation question """
+
+
+def island_perimeter(grid):
+    """ returns the perimeter of an island """
+    perimeter = 0
+    for i in range(1, len(grid) - 1):
+        for j in range(1, len(grid[i]) - 1):
+            if grid[i][j] == 1:
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+    return perimeter
